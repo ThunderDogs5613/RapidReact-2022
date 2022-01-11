f89d8488b8ad8b15e2fcf9837dbfcb7f4d507efd
stnick23 <nsbrinegar@gmail.com>
2022-01-11 00:15:56 -0700

    Add files not saved from previous commits

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,11 +7,9 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.DriveTrain.DriveTrain;
 import edu.wpi.first.wpilibj2.command.Command;
-
-import frc.robot.subsystems.DriveTrain;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -5,4 +5,7 @@ public class RobotMap {
  public static final int RIGHT_REAR_MOTOR = 01;
  public static final int LEFT_FRONT_MOTOR = 02;
  public static final int LEFT_REAR_MOTOR = 03;
+
+ public static final int DRIVE_STICK_ID = 0;
+ public static final int BUTTON_PAD_ID = 1;
 }
