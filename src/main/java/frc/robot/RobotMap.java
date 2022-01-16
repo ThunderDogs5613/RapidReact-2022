bd19d15a8b2d9d881db4406aafc614d024a1a4aa
stnick23 <nsbrinegar@gmail.com>
2022-01-16 14:22:59 -0700

    Correctly remap driveMotor CAN ID's

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,10 +1,10 @@
 package frc.robot;
 
 public class RobotMap {
- public static final int RIGHT_FRONT_MOTOR = 00;
- public static final int RIGHT_REAR_MOTOR = 01;
- public static final int LEFT_FRONT_MOTOR = 02;
- public static final int LEFT_REAR_MOTOR = 03;
+ public static final int RIGHT_FRONT_MOTOR = 01;
+ public static final int RIGHT_REAR_MOTOR = 02;
+ public static final int LEFT_FRONT_MOTOR = 03;
+ public static final int LEFT_REAR_MOTOR = 04;
 
  public static final int DRIVE_STICK_ID = 0;
  public static final int BUTTON_PAD_ID = 1;
