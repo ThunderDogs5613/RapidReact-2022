312f400b246083c153a5d2d1c4ac004fde53ce06
Android5613 <Yuengera@gmail.com>
2022-02-08 14:54:26 -0900

    Added robotmap values.

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -8,4 +8,10 @@ public class RobotMap {
 
  public static final int DRIVE_STICK_ID = 0;
  public static final int BUTTON_PAD_ID = 1;
+
+ public static final int INTAKE_ARM = 5;
+ public static final int INTAKE_ROLLER = 6;
+
+ public static final int INTAKE_ENCODER_1 = 0;
+ public static final int INTAKE_ENCODER_2 = 1;
 }
