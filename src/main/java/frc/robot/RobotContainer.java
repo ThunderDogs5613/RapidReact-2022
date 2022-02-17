8727a2cefba6022a8ad992d8e607bee8093c58c1
nicholas brinegar <nsbrinegar@gmail.com>
2022-02-16 20:14:09 -0700

    Remove unused import

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,7 +14,6 @@ import frc.robot.subsystems.Drivetrain.States.OpenLoopState;
 import frc.robot.ControllerMap.Logitech_Controller;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 
