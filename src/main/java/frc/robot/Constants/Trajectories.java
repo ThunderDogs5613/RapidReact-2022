42e447c38cdb1f3e7e21f24e27728169a8cb4a8a
nicholas brinegar <nsbrinegar@gmail.com>
2022-02-23 21:49:20 -0700

    Create trrajectories file

---
+++ src/main/java/frc/robot/Constants/Trajectories.java
@@ -0,0 +1,12 @@
+package frc.robot.Constants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+
+public class Trajectories {}
