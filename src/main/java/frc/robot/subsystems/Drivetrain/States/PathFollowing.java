2c6a7fba8000e0cad97b9336669a3c760d3acc58
nicholas brinegar <nsbrinegar@gmail.com>
2022-02-23 23:01:40 -0700

    finish constructor method for path following

---
+++ src/main/java/frc/robot/subsystems/Drivetrain/States/PathFollowing.java
@@ -0,0 +1,35 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.Drivetrain.States;
+
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain.DrivetrainSubsystem;
+
+public class PathFollowing extends CommandBase {    
+  
+  private Trajectory pathToFollow;
+
+  public PathFollowing(Trajectory trajectoryToFollow) {
+    addRequirements(DrivetrainSubsystem.getInstance());
+    pathToFollow = this.pathToFollow;
+    }
+
+  @Override
+  public void initialize() {
+
+  }
+
+  @Override
+  public void execute() {}
+
+  @Override
+  public void end(boolean interrupted) {}
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
