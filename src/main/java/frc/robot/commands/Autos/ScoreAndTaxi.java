2e4365e7bed06548368f59de0210a2561d314b35
nicholas brinegar <nsbrinegar@gmail.com>
2022-04-02 00:38:47 -0500

    Update scoreAndTaxi for new states

--- src/main/java/frc/robot/commands/Autos/ScoreAndTaxi.java
+++ src/main/java/frc/robot/commands/Autos/ScoreAndTaxi.java
@@ -5,6 +5,8 @@
 package frc.robot.commands.Autos;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.Constants.ArmConstants.ArmPosition;
+import frc.robot.subsystems.Arm.States.PositionState;
 import frc.robot.subsystems.CargoManipulator.States.YeetState;
 import frc.robot.subsystems.Drivetrain.States.SetAutonArcade;
 
@@ -14,8 +16,10 @@ public class ScoreAndTaxi extends SequentialCommandGroup {
     
     System.out.print("scoreAndDrive Autonomous");
     addCommands(
+      //new PositionState(ArmPosition.HIGH_CARGO).withTimeout(.1),
       new YeetState().withTimeout(4),
-      new SetAutonArcade(-.3, 0)
+      new SetAutonArcade(-.3, 0).withTimeout(2.5)
+      
     );
   }
 }
