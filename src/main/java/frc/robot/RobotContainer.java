d4cfd2f5e0677eab0cc8847582b0ad91c41b4eb1
cland29 <carl.lee.landskron@gmail.com>
2022-04-08 10:47:37 -0400

    Add a default so it would return something if no auto was selected

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -37,6 +37,7 @@ import frc.robot.commands.Autos.AutoChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 
@@ -108,6 +109,8 @@ public class RobotContainer {
         return PlainOLTaxi;
        case "Two Ball Auto":
         return TwoBallAuto;
+       default:
+        return ScoreAndTaxi;
       }
     //return AutoChooser.auto_chooser.getSelected();
   }
